131056a85cc0f4a4d2510233ec2f11e1795f255b
RemiIO Scarlet <i@remi.email>
2021-12-13 02:27:46 +0800

    Add output.

---
+++ output/_next/static/1Q97PjKsKAWsTZytMZG0l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/861-7eb29d88e4ed6769.js","static/chunks/pages/index-c79ddbec6080ee05.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
